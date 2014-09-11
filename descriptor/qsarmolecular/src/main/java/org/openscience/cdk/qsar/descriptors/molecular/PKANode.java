dc08cbaf66f09d89de1f368578d17ceb7c1a4367
Egon Willighagen <egonw@users.sourceforge.net>
2014-09-11 13:30:06 +0200

    Updated package assignments
    Change-Id: Iec168190c4246dda4ff4807b25e59872924624dc

--- descriptor/qsarmolecular/src/main/java/org/openscience/cdk/qsar/descriptors/molecular/PKANode.java
+++ descriptor/qsarmolecular/src/main/java/org/openscience/cdk/qsar/descriptors/molecular/PKANode.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package ambit2.descriptors;
+package org.openscience.cdk.qsar.descriptors.molecular;
 
 import java.util.Hashtable;
 
--- descriptor/qsarmolecular/src/test/java/org/openscience/cdk/qsar/descriptors/molecular/PKASmartsDescriptorTest.java
+++ descriptor/qsarmolecular/src/test/java/org/openscience/cdk/qsar/descriptors/molecular/PKASmartsDescriptorTest.java
@@ -20,8 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package ambit2.descriptors.test;
-
+package org.openscience.cdk.qsar.descriptors.molecular;
 
 import java.io.File;
 import java.io.FileOutputStream;
